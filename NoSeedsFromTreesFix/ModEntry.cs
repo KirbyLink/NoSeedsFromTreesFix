1fdee76e4b2a6436d4beca8eb10a5672adae13f9
KirbyLink <kjryan.reeves@gmail.com>
2018-10-16 11:11:07 -0700

    Changes to incorporate cross platform support

--- NoSeedsFromTreesFix/ModEntry.cs
+++ NoSeedsFromTreesFix/ModEntry.cs
@@ -24,14 +24,18 @@ namespace NoSeedsfromTreeFix
             //Harmony patcher
             //https://github.com/kirbylink/NoSeedsFromTreesFix.git
             var harmony = HarmonyInstance.Create("com.github.kirbylink.noseedsfromtreesfix");
-            harmony.PatchAll(Assembly.GetExecutingAssembly());
+            this.Apply(harmony);
+        }
+
+        public void Apply(HarmonyInstance harmony)
+        {
+            var original = typeof(Farmer).GetMethod("getEffectiveSkillLevel");
+            var postfix = typeof(PatchGetEffectiveSkillLevel).GetMethod("Postfix");
+            harmony.Patch(original, null, new HarmonyMethod(postfix));
         }
 
     }
 
-    [HarmonyPatch(typeof(Farmer))]
-    [HarmonyPatch("getEffectiveSkillLevel")]
-    [HarmonyPatch(new Type[] { typeof(int) })]
     public static class PatchGetEffectiveSkillLevel
     {
         static void Postfix(Farmer __instance, ref int whichSkill, ref int __result)
@@ -42,22 +46,20 @@ namespace NoSeedsfromTreeFix
                 Possible Fix: Line 1661 of Farmer.cs should be a simple decrement or -= 1 statement
                 instead of using the newLevels.Y value
             */
-            if (whichSkill >= 0 && whichSkill <= 5)
+            if (whichSkill < 0 || whichSkill > 5) return;
+            int[] numArray = new int[6]
             {
-                int[] numArray = new int[6]
-                {
-                    (int) (NetFieldBase<int, NetInt>) __instance.farmingLevel,
-                    (int) (NetFieldBase<int, NetInt>) __instance.fishingLevel,
-                    (int) (NetFieldBase<int, NetInt>) __instance.foragingLevel,
-                    (int) (NetFieldBase<int, NetInt>) __instance.miningLevel,
-                    (int) (NetFieldBase<int, NetInt>) __instance.combatLevel,
-                    (int) (NetFieldBase<int, NetInt>) __instance.luckLevel
-                };
+                __instance.farmingLevel.Value,
+                __instance.fishingLevel.Value,
+                __instance.foragingLevel.Value,
+                __instance.miningLevel.Value,
+                __instance.combatLevel.Value,
+                __instance.luckLevel.Value
+            };
 
-                for (int i = 0; i < __instance.newLevels.Count; ++i)
-                    numArray[__instance.newLevels[i].X]--;
-                __result = numArray[whichSkill];
-            }
+            for (int i = 0; i < __instance.newLevels.Count; ++i)
+                numArray[__instance.newLevels[i].X]--;
+            __result = numArray[whichSkill];
         }
     }
 }
